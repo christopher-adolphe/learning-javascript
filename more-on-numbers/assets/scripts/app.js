1aa0b9ea725efd09face6d21da54d14831e1f028
cadolphe <christopher.adolphe@gmail.com>
2021-10-28 07:51:35 +0400

    Understanding tagged templates and regular expressions

--- more-on-numbers/assets/scripts/app.js
+++ more-on-numbers/assets/scripts/app.js
@@ -50,3 +50,69 @@ console.log('Mixing 450n to 12: TypeError: Cannot mix BigInt and other types, us
 console.log('Converting 450n to a number: ', parseInt(450n));
 // Converting a number into a BigInt
 console.log('Converting 12 to a BigInt: ', BigInt(12));
+
+// The Global Number Object
+console.log('--- The Global Number Object ---');
+// Check if a number is finite
+console.log('Is 10 finite: ', Number.isFinite(10));
+console.log('Is 1/0 finite: ', Number.isFinite(1/0));
+
+// The Global Math Object
+console.log('--- The Global Math Object ---');
+const randomInRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+console.log('Generate a random number between 5 & 35: ', randomInRange(5, 35));
+
+// Strings
+console.log('--- Strings ---');
+const firstName = 'Jade';
+console.log(`Hello ${firstName}`);
+
+// Template tag allows to concatenate a template literal to a function
+// which then execute the function and uses the concatenated template literal
+// as parameters to the function. The first parameter will be an array of the
+// concatenated template literal splitted by whitespace and the remaining parameters
+// will be any dynamic value passed in the concatenated template literal.
+const product = {
+  name: 'Mountain Bike',
+  price: 24000
+};
+
+const productDescription = (strings, prodName, prodPrice) => {
+  console.log('productDescription: ', strings);
+  console.log('productDescription: ', prodName);
+  console.log('productDescription: ', prodPrice);
+  let priceCategory = 'cheap';
+
+  if (prodPrice > 10000) {
+    priceCategory = 'expensive';
+  }
+
+  return `${strings[0]}${product.name}${strings[1]}${priceCategory}${strings[2]}`
+};
+
+const description = productDescription`This product (${product.name}) is ${product.price}.`;
+console.log('Description: ', description);
+
+// Regular Expressions
+console.log('--- Regular Expressions ---');
+// Regular expressions are used to search for patterns in strings
+const regex = /^\S+@\S+\.\S+$/
+// Understanding the different parts of the above regular expression
+// The first and last forward slashes / denotes that this is a regular expression
+// The ^ defined the beginning of the tested string (starting from the left)
+// The \S+ implies any kind of word
+// The @ implies that the tested string should contain this character
+// The \. implies that the tested string should contain a period. The back slash is used here to escape the period in the regEx
+// The $ defines the end of the test string
+
+// A regular expression pattern can also be created using the RegExp constructor function
+
+let userInput = 'someemail@gmail.com';
+
+// Using the test() method to search the pattern in the provided string
+console.log('Is userInput valid: ', regex.test(userInput));
+
+userInput = 'someemailgmail.com';
+console.log('Is userInput valid: ', regex.test(userInput));
