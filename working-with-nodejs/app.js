d21a3d2a360b6773097365f941c49f1ddb6fb1ae
cadolphe <christopher.adolphe@gmail.com>
2021-11-15 08:25:01 +0400

    Understanding express js framework

--- working-with-nodejs/app.js
+++ working-with-nodejs/app.js
@@ -1,44 +1,55 @@
-// Import the HTTP module to get access to its API
-const http = require('http');
+// Importing the express module
+const express = require('express');
 
-// Using the 'createServer()' method from the HTTP API to build a server
-const server =http.createServer((request, response) => {
-  let body = [];
+// Using the 'express()' top-level function to create an Express application
+// The 'app' object now has access to the API exposed by express
+const app = express();
 
-  // Using the 'on()' method to add an event listener to the 'data' event on the request
-  request.on('data', (chunk) => {
-    body.push(chunk);
-  });
+// Using the 'set()' method to configure the Express application
+// Setting a config for the templating engine to be used for our views
+// Specifying we want to use 'ejs' as templating engine
+app.set('view engine', 'ejs');
 
-  // Adding a listener to the 'end' event to trigger an action when the server is done
-  // with receiving data
-  request.on('end', () => {
-    let username = '';
+// Specifying the directory where our views are found, here in 'templates' directory
+app.set('views', 'templates');
 
-    // Converting the array of data chunks to a string
-    body = Buffer.concat(body).toString();
-    
-    if (body) {
-      username = body.split('=')[1];
-    }
+// Using the 'use()' method to register a middleware. Express is a middleware
+// driven framework meaning that the incoming request will be funneled to a bunch
+// of functions which all receive the request and all can do something with it.
+// They can either stop the request and send back a response or they can forward
+// the request to the next middleware function
 
-    // Setting header to the outgoing response
-    response.setHeader('Content-Type', 'text/html');
-    
-    // Writing some data into the response stream
-    response.write(`
-      <h1>Hello from Nodejs</h1>
-      <form method="POST" action="/">
-        <input type="text" name="username" />
-        <button type="submit">Send</button>
-      </form>
-      ${ username !== '' ? `<p>Your username: ${username} has been added!</p>` : ''}
-    `);
-    
-    // Signaling the no more data will be written
-    response.end();
+// Registering a middleware function to parse the body of the incoming request
+app.use(express.urlencoded({ extended: false }));
+
+app.use((request, response, next) => {
+  response.set('Content-Type', 'text/html');
+  // Using the 'next()' callback to pass the incoming request to the next
+  // middleware function
+  next();
+});
+
+app.use((request, response, next) => {
+  // Extracting the data from the incoming request body which has been parsed by
+  // the previous middleware function 'app.use(express.urlencoded())'
+  const username = request.body.username || '';
+
+  // Using the 'send()' method to send the response to the client side
+  // NOTE: The incoming request is no more passed to the next middleware function
+  // response.send(`
+  //   <h1>Hello from Express</h1>
+  //   <form method="POST" action="/">
+  //     <input type="text" name="username" />
+  //     <button type="submit">Send</button>
+  //   </form>
+  //   ${ username !== '' ? `<p>Your username: ${username} has been added!</p>` : ''}
+  // `);
+
+  // USing the 'render()' method to specify the view we want to send in the response
+  response.render('index', {
+    username
   });
 });
 
-// Using the 'listen()' method on the server object to start it by supplying a port number
-server.listen(3000, () => console.log(`Server started on port 3000... Visit http://localhost:3000`));
+// Using the 'listen()' method to start the server
+app.listen(3000, () => console.log(`Server started on port 3000... Visit http://localhost:3000`))
