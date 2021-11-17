aca2d02d16bb9efe1736c6b237833775a45c2b50
cadolphe <christopher.adolphe@gmail.com>
2021-11-17 08:53:52 +0400

    Added mongodb to nodejs application for simple CRUD operations

--- working-with-nodejs/routes/location.js
+++ working-with-nodejs/routes/location.js
@@ -1,38 +1,94 @@
 const express = require('express');
+
+// Importing mongodb module
+const mongodb = require('mongodb');
+
+// Importing MongoClient module
+const MongoClient = mongodb.MongoClient;
+
 // Creating an Express Router which will be used to register different routes
 const router = express.Router();
+
 // Creating a temporary storage for the locations
 const locationStorage = {
   locations: []
 };
 
+const uri = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+
+// Creating a new mongo client
+const client = new MongoClient(uri);
+
 // Registering a POST request on '/add-location' path
 router.post('/add-location', async (request, response, next) => {
-  const id = Math.random();
+  // const id = Math.random();
   const { address, lat, lng } = request.body;
 
-  locationStorage.locations.push({
-    id,
-    address,
-    coordinates: { lat, lng }
-  });
+  // locationStorage.locations.push({
+  //   id,
+  //   address,
+  //   coordinates: { lat, lng }
+  // });
 
-  console.log('locationStorage: ', locationStorage);
+  // console.log('locationStorage: ', locationStorage);
+  try {
+    // Connecting the client to the server
+    await client.connect();
 
-  response.json({ message: 'Location successfully saved!', locationId: id });
+    // Creating a new Db instance to the database we want
+    const db = await client.db('share-place');
+
+    // Inserting the document (i.e the new record) in the locations table
+    const result = await db.collection('locations').insertOne({ address, coordinates: { lat, lng }});
+
+    console.log('add location result: ', result);
+
+    response.json({ message: 'Location successfully saved!', locationId: result.insertedId });
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    // Ensuring that the client will close after we are done or in case of failure
+    await client.close();
+  }
 });
 
-router.get('/location/:locationId', (request, response, next) => {
-  const locationId = +request.params.locationId;
-  const location = locationStorage.locations.find(location => location.id === locationId);
+router.get('/location/:locationId', async (request, response, next) => {
+  let locationId = request.params.locationId;
 
-  if (!location) {
-    return response.status(404).json({ message: 'Sorry, location not found!' });
+  if (!locationId) {
+    return response.status(404).json({ message: 'Sorry, location id not provided!' });
   }
 
-  const { address, coordinates } = location;
+  // const location = locationStorage.locations.find(location => location.id === locationId);
 
-  response.json({ address, coordinates });
+  try {
+    locationId = new mongodb.ObjectId(locationId);
+  } catch (error) {
+    return response.status(500).json({ message: 'Sorry, location id provided is invalid!' });
+  }
+  console.log('locationId: ', locationId);
+
+  try {
+    await client.connect();
+
+    const db = await client.db('share-place');
+
+    const result = await db.collection('locations').findOne({ _id: locationId });
+
+    console.log('retrieved location result: ', result);
+
+    if (!result) {
+      return response.status(404).json({ message: 'Sorry, location not found!' });
+    }
+
+    const { address, coordinates } = result;
+
+    response.json({ address, coordinates });
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    await client.close();
+  }
 });
 
 router.get('/', (request, response, next) => {
