cadc7389a892d21aed89ca000230ca5b767f59c7
cadolphe <christopher.adolphe@gmail.com>
2021-12-05 12:38:49 +0400

    Added web component styling and attribute changes

--- web-components/basics/tooltip.js
+++ web-components/basics/tooltip.js
@@ -1,5 +1,11 @@
 // Building an autonomous custom element
 class Tooltip extends HTMLElement {
+  // Creating a getter for the `observedAttributes` property to set a list
+  // of attributes we wish to observe on the custom component
+  static get observedAttributes() {
+    return ['content'];
+  }
+
   // Element created
   constructor() {
     super();
@@ -12,6 +18,8 @@ class Tooltip extends HTMLElement {
     // as a private property meaning that it should not be
     // accessed/used outside the class
     this._tooltipContainer;
+
+    this._tooltipIcon;
 
     // Defining a property to store the tooltip text content and
     // initializing it a default value
@@ -32,6 +40,16 @@ class Tooltip extends HTMLElement {
     // Using the innerHTML property to creating the template within the class
     // Setting the innerHTML property inside the class also allows to add
     // scoped style to the custom element
+
+    // Using the :host selector to apply styles to the custom component
+
+    // Using the :host-context selector to apply styles to the custom component
+    // under specific usage context, e.g: here the font weight will be bold only
+    // when the custom component is nested inside a parent element having .danger
+    // class
+
+    // Using the ::slotted selector to apply styles to content projected
+    // inside the custom component
     this.shadowRoot.innerHTML = `
       <style>
         div {
@@ -41,10 +59,26 @@ class Tooltip extends HTMLElement {
           width: 400px;
           max-width: 400px;
           padding: 0.5rem;
-          background-color:#8e44ad;
+          background-color: #8e44ad;
           color: #ecf0f1;
           border-radius: 4px;
           z-index: 10;
+        }
+
+        :host {
+          position: relative;
+        }
+
+        :host(.warning) {
+          background-color: var(--warning-color, #2980b9);
+        }
+
+        :host-context(.danger) {
+          font-weight: bold;
+        }
+
+        ::slotted(.highlight) {
+          border-bottom: 2px dashed var(--highlight-border-color);
         }
       </style>
       <slot>Default tooltip slot</slot><span> (?)</span>
@@ -55,7 +89,7 @@ class Tooltip extends HTMLElement {
   // to start DOM initialization. This hook allows
   // us to access/add elements in the DOM
   connectedCallback() {
-    const tooltipIcon = this.shadowRoot.querySelector('span');
+    this._tooltipIcon = this.shadowRoot.querySelector('span');
 
     // Checking if the `content` attribute has been set on the
     // custom element to override the default value of the
@@ -64,23 +98,46 @@ class Tooltip extends HTMLElement {
       this._tooltipContent = this.getAttribute('content');
     }
 
-    tooltipIcon.textContent = ' (?)';
-    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this._tooltipIcon.textContent = ' (?)';
+    this._tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
 
     // Applying some basic styles to the custom element
-    this.style.position = 'relative';
+    // this.style.position = 'relative';
 
-    this.shadowRoot.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(this._tooltipIcon);
   }
 
   // Using the `disconnectedCallback()` lifecycle hook
   // to do cleanup work when the element is detached
   // from the DOM
+  disconnectedCallback() {
+    console.log('disconnectedCallback called');
+
+    this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip);
+    this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip);
+  }
 
   // Using the `attributeChangedCallback()` lifecycle hook
   // to observe changes on attribute so that we can update
-  // data or DOM elements within the custom element
+  // data or DOM elements within the custom element. It takes
+  // 3 arguments; 1st the name of the attribute to observe, 
+  // 2nd the old value of the observed attribute and 3rd the
+  // new value of the observed attribute
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('attributeChangedCallback: ', { name, oldValue, newValue });
+
+    // Checking if the value of the attribute has changed
+    if (newValue === oldValue) {
+      return;
+    }
+
+    // Checking which attribute has changed in case we are
+    // observing multiple attributes
+    if (name === 'content') {
+      this._tooltipContent = newValue;
+    }
+  }
 
   // Creating a `showTooltip()` method that will handle the
   // display of the tooltip on mouse enter event. We are using
